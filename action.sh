683ae590bd231683189ef2f413a346c0288ac520
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-02-20 23:18:21 +0100

    Revert "fix?"
    This reverts commit 4ac626e8b4dbfc4113c10d735ec3f615a7cdef90.

--- action.sh
+++ action.sh
@@ -36,7 +36,6 @@ sandbox() {
         --ro-bind /nix/store /nix/store \
         --ro-bind /nix/var/nix /nix/var/nix \
         --ro-bind /etc/resolv.conf /etc/resolv.conf \
-        --ro-bind /etc/passwd /etc/passwd \
         --clearenv \
         --unshare-pid \
         $1
